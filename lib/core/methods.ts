81d31ad4d757f27a8bba1cf5e847274933df0da2
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-17 00:04:21 -0400

    feat: add ETECSA methods extraction from virtual DOM

---
+++ lib/core/methods.ts
@@ -0,0 +1,17 @@
+import { loadVirtualDOM } from './dom';
+import { AxiosStatic } from 'axios';
+
+export const ETECSA: {
+  axios: AxiosStatic;
+  encryptPayload: (data: object) => { datos: [string, string] };
+  href: string;
+  CryptoJS: typeof CryptoJS;
+} = {} as any;
+
+/**
+ *
+ */
+export const initEtecsaMethods = async () => {
+  const dom = await loadVirtualDOM();
+  Object.assign(ETECSA, dom.window.extracted);
+};
