537d85031948dd0d6b34a9467e1b8e9dfcf9fe43
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-19 05:18:34 -0400

    fix(consts): replace __dirname  for ESM compatibility

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SERVER_API = 'https://www.tienda.etecsa.cu';
-export const LIB_FOLDER = __dirname;
+export const LIB_FOLDER = import.meta.dirname;
 export const OFFLINE_SERVER_PORT = 3256;
